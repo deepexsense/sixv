474bac40a3cb5deaab8c46808cc35b8d475b3904
deepexsense <dashka2h@yandex.ru>
2018-11-16 10:55:07 +0300

    New styles and HTMLs for project

--- untitled/urls.py
+++ untitled/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^sixv/', include('sixv.urls')),
+    url(r'^', include('sixv.urls')),
     url(r'^admin/', admin.site.urls),
 ]
 
